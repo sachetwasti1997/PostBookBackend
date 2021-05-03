60e1839df76cdaf0872979ed39f4bbb9f55e53f9
sachetwasti1997 <sachetwasti61@gmail.com>
2021-05-03 15:03:06 +0530

    [UPDATE] Added Custom Validators for Errors

---
+++ src/main/java/com/sachet/postBook/security/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.sachet.postBook.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+}
