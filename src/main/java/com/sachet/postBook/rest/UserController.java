a0a603add10693763aae80991a334c8863a9abb4
sachetwasti1997 <sachetwasti61@gmail.com>
2021-05-03 08:49:23 +0530

    [UPDATE] Added NotNull Size annotations

--- src/main/java/com/sachet/postBook/rest/UserController.java
+++ src/main/java/com/sachet/postBook/rest/UserController.java
@@ -3,7 +3,13 @@ package com.sachet.postBook.rest;
 import com.sachet.postBook.model.User;
 import com.sachet.postBook.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
 
 @RestController
 @CrossOrigin
@@ -17,8 +23,10 @@ public class UserController {
     }
 
     @PostMapping("/api/1.0/users/save")
-    void createUsers(@RequestBody User user){
+    public ResponseEntity<?> createUsers(@Valid @RequestBody User user){
+
         User userSaved = userService.save(user);
+        return new ResponseEntity<>(userSaved, HttpStatus.OK);
     }
 
 }
--- src/test/java/com/sachet/postBook/UserControllerTest.java
+++ src/test/java/com/sachet/postBook/UserControllerTest.java
@@ -18,6 +18,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -35,7 +37,7 @@ public class UserControllerTest {
         User user = new User();
         user.setUserName("test_user_name");
         user.setDisplayName("test_display_name");
-        user.setPassword("test_password");
+        user.setPassword("test_password4V@");
         user.setEmail("test@gmail.com");
         return user;
     }
@@ -45,11 +47,15 @@ public class UserControllerTest {
         userService.deleteAll();
     }
 
+    public <T> ResponseEntity<?> postSignUp(Object request, Class<T> response){
+        return testRestTemplate.postForEntity(API_USER, request, response);
+    }
+
     @Test
     public void postUser_userValid_receiveOk(){
         User user = createValidUser();
 
-        ResponseEntity<Object> response = testRestTemplate.postForEntity(API_USER, user, Object.class);
+        ResponseEntity<?> response = postSignUp(user, Object.class);
         Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
     }
 
@@ -57,17 +63,128 @@ public class UserControllerTest {
     public void postUser_userValid_userSavedToDatabase(){
         User user = createValidUser();
 
-        ResponseEntity<Object> response = testRestTemplate.postForEntity(API_USER, user, Object.class);
+        ResponseEntity<?> response = postSignUp(user, Object.class);
         Assertions.assertEquals(userService.count(), 1);
+    }
+
+    @Test
+    public void postUser_userInvalid_userNameNull(){
+        User user = createValidUser();
+        user.setUserName(null);
+
+        ResponseEntity<?> response = postSignUp(user, Object.class);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void postUser_userInvalid_displayNameNull(){
+        User user = createValidUser();
+        user.setDisplayName(null);
+
+        ResponseEntity<?> response = postSignUp(user, Object.class);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void postUser_userInvalid_passwordNull(){
+        User user = createValidUser();
+        user.setPassword(null);
+
+        ResponseEntity<?> response = postSignUp(user, Object.class);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void postUser_userInvalid_emailNull(){
+        User user = createValidUser();
+        user.setEmail(null);
+
+        ResponseEntity<?> responseEntity = postSignUp(user, Object.class);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
     }
 
     @Test
     public void postUser_userValid_checkPasswordHashed(){
         User user = createValidUser();
-        testRestTemplate.postForEntity(API_USER, user, Object.class);
+        postSignUp(user, Object.class);
         List<User> users = userService.findAll();
         User user1 = users.get(0);
         Assertions.assertNotEquals(user1.getPassword(), user.getPassword());
+    }
+
+    @Test
+    public void postUser_userInvalid_userNameLengthLess(){
+        User user = createValidUser();
+        user.setUserName("abc");
+
+        ResponseEntity<?> response = postSignUp(user, Object.class);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void postUser_userInvalid_displayNameLengthLess(){
+        User user = createValidUser();
+        user.setDisplayName("abc");
+
+        ResponseEntity<?> response = postSignUp(user, Object.class);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void postUser_userInvalid_userNameLengthMore(){
+        User user = createValidUser();
+        String valueOf256Chars = IntStream.rangeClosed(1, 256).mapToObj(x -> "a").collect(Collectors.joining());
+        user.setDisplayName(valueOf256Chars);
+
+        ResponseEntity<?> response = postSignUp(user, Object.class);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void postUser_userInvalid_displayNameLengthMore(){
+        User user = createValidUser();
+        String valueOf256Chars = IntStream.rangeClosed(1, 256).mapToObj(x -> "a").collect(Collectors.joining());
+        user.setDisplayName(valueOf256Chars);
+
+        ResponseEntity<?> response = postSignUp(user, Object.class);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void postUser_userInvalid_passwordLengthLess(){
+        User user = createValidUser();
+        user.setPassword("aA1]");
+
+        ResponseEntity<?> response = postSignUp(user, Object.class);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void postUser_userInvalid_passwordLengthMore(){
+        User user = createValidUser();
+        String valueOf256Chars = IntStream.rangeClosed(1, 256).mapToObj(x -> "a").collect(Collectors.joining());
+        user.setPassword(valueOf256Chars+"aA1@");
+
+        ResponseEntity<?> response = postSignUp(user, Object.class);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void postUser_userInvalid_passwordWithAllLowerCase(){
+        User user = createValidUser();
+        user.setPassword("aaaaaaa");
+
+        ResponseEntity<?> response = postSignUp(user, Object.class);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void postUser_userValid_passwordCorrect(){
+        User user = createValidUser();
+        user.setPassword("aAA1@");
+
+        ResponseEntity<?> response = postSignUp(user, Object.class);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
 }
